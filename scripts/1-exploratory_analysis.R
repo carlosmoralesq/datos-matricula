f0ef98ee62c66679c97b8cbe74612aeb7e864da4
Matías Castillo Aguilar <matiascastilloaguilar@MacBook-Pro-de-Matias.local>
2021-08-26 20:14:39 -0400

    Más legible y reproducible

--- scripts/1-exploratory_analysis.R
+++ scripts/1-exploratory_analysis.R
@@ -4,7 +4,9 @@
 # Instalamos paquetes (si no están en el sistema)
 # install.packages("data.table")
 # install.packages("ggplot2")
-# install.packages("gtsummary")
+# install.packages("GGally")
+# install.packages("dplyr")
+# install.packages("correlation")
 
 # Cargamos paquetes
 library(data.table)
@@ -31,32 +33,43 @@ temp <- melt(data, measure.vars = c("matricula_coanil", "matricula_pie"))
 
 # 1. Distribución de la edad de los alumnos agrupado por región
 ggplot(temp, aes(x = año, y = (value), fill = año)) +
-  facet_wrap(~variable, nrow = 2, scales = "free_y") +
-  # Etiquetas de los ejessfvdfbvsdvjnsv
+  facet_wrap(~variable, nrow = 2, scales = "free_y", ) +
+  # Etiquetas de los ejes
   labs(x = "Año", y = "Matrículas") + 
   # Gráfico de violín - primera capa
   geom_violin(alpha = .3) + 
   # Gráfico de cajas - segunda capa
-  geom_boxplot(width = .1) +
+  geom_boxplot(width = .1, outlier.colour = NA, notch = TRUE) +
   # Jitter (puntos) - tercera capa
   geom_jitter(cex = .05, width = .03) +
-  # Barras de error usando intervalo de confianza de la media mediantsfvdfbvsdvjnsve Bootstrap - cuarta capa
-  # stat_summary(geom = "errorsfvdfbvsdvjnsvbar", fun.data = mean_cl_boot, col = "white", width = .2) +
+  # Barras de error usando intervalo de confianza de la media mediante Bootstrap - cuarta capa
+  stat_summary(geom = "errorbar", fun.data = mean_cl_boot, col = "white", width = .1) +
   # Punto representando la media aritmética - quinta capa
-  # stat_summary(geom = "point", fun = mean, col = "white") +
-  # Tema clásico (formato pubsfvdfbvsdvjnsvlicación)
+  stat_summary(geom = "point", fun = mean, col = "white") +
+  # Tema clásico (formato publicación)
   theme_classic()
 
 # 2. Matriz de correlación
 GGally::ggpairs(data, cardinality_threshold = 16)
 
+# # Matriz de correlación de ejemplo
 GGally::ggpairs(iris, mapping = aes(fill = Species), alpha = .5)
 
+
+# Correlaciones -------------------------------------------------------------------------------
+
+# Usando sintaxis de data.table
+data[j = correlation::correlation(.SD), keyby = "region"][p < 0.05, .(paste(Parameter1, "y", Parameter2), r), "region"]
+# Pros: Más rápido, más conciso
+# Contras: Código más complejo
+
+
+# Usando dplyr - tidyverse
 library(dplyr)
 
 data %>% 
   group_by(region) %>%
   correlation::correlation(method = "spearman") %>%
   filter(p > 0.05) 
-
-data[j = correlation::correlation(.SD), keyby = "region"][p < 0.05, .(paste(Parameter1, "y", Parameter2), r), "region"]
+# Pros: Fácil entender (more 'verbose') por su naturaleza secuencial
+# Contra: Lento en casos de datos muy grandes (1 Millon de filas)
